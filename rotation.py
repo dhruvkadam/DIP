24573caf636582fda99c3de8753c6db26b2ec485
dhruvkadam <59744459+dhruvkadam@users.noreply.github.com>
2021-09-30 15:51:08 +0530

    Add files via upload

---
+++ rotation.py
@@ -0,0 +1,25 @@
+import cv2 as cv
+img = cv.imread('C:/Users/Dhruv/Desktop/MT/gg.jpg',0)
+rows,cols = img.shape
+
+M = cv.getRotationMatrix2D(((cols-1)/2.0,(rows-1)/2.0),90,1)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+dst = cv.warpAffine(img,M,(cols,rows))
+cv.imshow('img',dst)
+cv.waitKey(0)
+cv.destroyAllWindows()
